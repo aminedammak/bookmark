66069033f7a824fc70af67b8c740357aebb763c2
amine.dammak <amine.dammak@foodomarket.com>
2024-12-28 15:18:04 +0100

    jwt startegy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -54,7 +54,10 @@ export class AuthService {
     return this.signToken(user.id, user.email);
   }
 
-  signToken(userId: number, email: string): Promise<string> {
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
     const payload = {
       sub: userId,
       email,
@@ -62,6 +65,10 @@ export class AuthService {
 
     const secret = this.config.get('JWT_SECRET');
 
-    return this.jwt.signAsync(payload, { expiresIn: '15m', secret: secret });
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+    return { access_token: token };
   }
 }
